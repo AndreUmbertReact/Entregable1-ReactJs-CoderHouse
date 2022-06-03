2e91cdb70df76ee6df1632b8ce6c091053fe0be3
ANDRUPA <andrupa_umbert@hotmail.com>
2022-06-03 15:34:08 -0300

    creating reactjs app
    starting my app for reactjs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pixel Art Shop</h1>
+      <p>Umbert André</p>
+    </div>
+  );
+}
+
+export default App;
